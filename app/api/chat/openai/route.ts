a9564004c4c4b024c57b8379d80e0211d63dad6a
mahesh <maheshpalakunda.com>
2026-04-17 16:03:18 +0530

    fix: openai route dynamic

--- app/api/chat/openai/route.ts
+++ app/api/chat/openai/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { checkApiKey, getServerProfile } from "@/lib/server/server-chat-helpers"
 import { ChatSettings } from "@/types"
 import { OpenAIStream, StreamingTextResponse } from "ai"
